979b01c7c9f4b070e391948358c7c8b88a158a63
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-09-29 22:48:30 +0900

    Update 4_3_C++_OOP_Gaussian.cpp

--- 4_3_C++_OOP_Gaussian.cpp
+++ 4_3_C++_OOP_Gaussian.cpp
@@ -1,31 +1,5 @@
 #include <math.h>       /* sqrt, exp */
-
-// class declaration
-class Gaussian
-{
-	private:
-		float mu, sigma2;
-
-	public:
-		
-		// constructor functions
-		Gaussian ();
-		Gaussian (float, float);
-
-		// change value of average and standard deviation 
-		void setMu(float);
-		void setSigma2(float);
-
-		// output value of average and standard deviation
-		float getMu();
-		float getSigma2();
-
-		// functions to evaluate 
-		float evaluate (float);
-		Gaussian mul (Gaussian);
-		Gaussian add (Gaussian);
-};
-
+#include "gaussian.h"
 
 Gaussian::Gaussian() {
 	mu = 0;
@@ -37,29 +11,12 @@ Gaussian::Gaussian (float average, float sigma) {
 	sigma2 = sigma;
 }
 
-void Gaussian::setMu (float average) {
-	mu = average;
-}
-
-void Gaussian::setSigma2 (float sigma) {
-	sigma2 = sigma;
-}
-
-
-float Gaussian::getMu () {
-	return mu;
-}
-
-float Gaussian::getSigma2() {
-	return sigma2;
-}
-
 float Gaussian::evaluate(float x) {
 	float coefficient;
 	float exponential;
 
 	coefficient = 1.0 / sqrt (2.0 * M_PI * sigma2);
-	exponential = exp ( -0.5 * pow ((x - mu), 2) / sigma2 );
+	exponential = exp ( pow (-0.5 * (x - mu), 2) / sigma2 );
 	return coefficient * exponential;
 }
 
@@ -69,8 +26,8 @@ Gaussian Gaussian::mul(Gaussian other) {
 	float new_mu;
 	float new_var;
 
-	denominator = sigma2 + other.getSigma2();
-	numerator = mu * other.getSigma2() + other.getMu() * sigma2;
+	denominator = sigma2 + other.sigma2;
+	numerator = mu * other.sigma2 + other.mu * sigma2;
 	new_mu = numerator / denominator;
 
 	new_var = 1.0 / ( (1.0 / sigma2) + (1.0 / other.sigma2) );
@@ -83,8 +40,8 @@ Gaussian Gaussian::add(Gaussian other) {
 	float new_mu;
 	float new_sigma2;
 
-	new_mu = mu + other.getMu();
-	new_sigma2 = sigma2 + other.getSigma2();
+	new_mu = mu + other.mu;
+	new_sigma2 = sigma2 + other.sigma2;
 
 	return Gaussian(new_mu, new_sigma2);
 }
