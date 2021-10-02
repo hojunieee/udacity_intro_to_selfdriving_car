5eefec4f40fbccf5eb105fa1a017a40f017a24bd
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-02 09:29:12 +0900

    Update 4_3_C++_OOP_main.cpp

--- 4_3_C++_OOP_main.cpp
+++ 4_3_C++_OOP_main.cpp
@@ -1,62 +1,95 @@
-#include <iostream>
-#include "4_3_C++_OOP_Gaussian.h"
-// class declaration
+#include "matrix.h"
 
+Matrix::Matrix() {
+    std::vector <std:: vector <float> > initial_grid (10, std::vector <float>(5, 0.5));
+    grid = initial_grid;
+    rows = initial_grid.size();
+    cols = initial_grid[0].size();
 
-int main ()
-{
- /*
-TODO: Instantiate a Gaussian object called gaussianone.The object
-should have mean = 40.0 and variance (aka sigma2) = 225.0
-*/
-    Gaussian gaussianone(40,225.0);
-
-/*
-TODO: Instantiate another Gaussian object called gaussiantwo. 
-The object should have mean = 35.6 and variance = 12.25
-*/
-    Gaussian gaussiantwo(35.6,12.25);
-
-/*
-TODO:
-Output to the terminal the following (hint: use the std namespace with cout or
-use std::cout):
-- the probability density function value for gaussianone when x = 10.5
-- the probability density function value for gaussianone when x = 55.4
-- the probability density function value for gaussiantwo when x = 35.6
-- the probability density function value for gaussiantwo when x = 29.4
-*/
-    std::cout <<gaussianone.evaluate(10.5) << "\n";
-    std::cout <<gaussianone.evaluate(55.4) << "\n";
-    std::cout <<gaussiantwo.evaluate(35.6) << "\n";
-    std::cout <<gaussiantwo.evaluate(29.4) << "\n";
-    
-/*
-TODO:
-- Change the mean value of gaussianone to mean = 45
-- Change the variance of gaussiantwo to variance = 15.4
-- Output the mean of gaussianone to the terminal
-- Output the variance of gaussiantwo to the terminal
-*/
-    gaussianone.setMu(45);
-    gaussiantwo.setSigma2(15.4);
-    std::cout << gaussianone.getMu() << "\n";
-    std::cout << gaussiantwo.getSigma2() << "\n";
-    
-/*
-TODO:
-- Multiply gaussian one and gaussian two. Store the resulting gaussian
-in a variable called gaussianthree
-- Output the mean and variance of gaussianthree to the terminal
-- Add gaussian one and gaussian two. Store the resulting gaussian in a 
-variable called gaussianfour
-- Output the mean and variance of gaussianfour to the terminal
-*/
-    Gaussian gaussianthree = gaussianone.mul(gaussiantwo);
-    std::cout << gaussianthree.getMu() << "\n";
-    std::cout << gaussianthree.getSigma2() << "\n";
-    
-    Gaussian gaussianfour = gaussianone.add(gaussiantwo);
-    std::cout << gaussianfour.getMu() << "\n";
-    std::cout << gaussianfour.getSigma2() << "\n";
 }
+
+Matrix::Matrix(std::vector <std:: vector <float> > initial_grid) {
+    grid = initial_grid;
+    rows = initial_grid.size();
+    cols = initial_grid[0].size();
+}
+
+
+void Matrix::setGrid(std::vector< std::vector<float> > new_grid) {
+    grid = new_grid;
+    rows = new_grid.size();
+    cols = new_grid[0].size();
+}
+
+std::vector< std::vector<float> > Matrix::getGrid() {
+    return grid;
+}
+
+std::vector<int>::size_type Matrix::getRows() {
+    return rows;
+}
+
+std::vector<int>::size_type Matrix::getCols() {
+    return cols;
+}
+
+/* TODO: Define a matrix_addition function
+**   INPUT: a matrix
+**   OUPUT: the sum of the grid variable and the input matrix
+**
+** STEPS:
+**  1. check that the matrix in the grid variable
+**     and the input matrix have the same size
+**
+**     if not, throw an error like
+**         throw std::invalid_argument("matrices are not the same size");
+**
+** 2. add the matrices together and return
+**       the result as a Matrix. You can do this part
+**       with nested for loops. If you use an intermediate
+**       vector to store a row, the vector.clear() 
+**       method might be useful.
+**
+*/
+Matrix Matrix::matrix_addition(Matrix other) {
+    
+    std::vector< std::vector<float> > othergrid = other.getGrid();
+
+    std::vector< std::vector<float> > result;
+
+    std::vector<float> new_row;
+    
+    if (rows != other.getRows() || (cols != other.getCols())){
+        throw std::invalid_argument("matrices are not the same size");
+    }
+    
+    else{
+        for (int row=0;row<rows;rows++){
+            new_row.clear();
+            for (int column=0;column<cols;column++){
+                new_row.push_back(grid[row][column] + othergrid[row][column]);
+            }
+            result.push_back(new_row);
+        } 
+    }
+}
+
+void Matrix::matrix_print(){
+    for (int y=0;y<rows;y++){
+        for (int x=0;x<cols;x++){
+            std::cout<<grid[y][x]<<" ";
+        }
+        std::cout << std::endl;
+    }
+}
+/* TODO: Define a matrix_print function
+** This function has no inputs and no outputs
+** The purpose of the function is to display the matrix in
+** the terminal using std::cout. 
+**
+**
+** The output should look something like:
+** 4    9   1   10
+** 5    11  6   17
+** 8    4   15  2
+*/
