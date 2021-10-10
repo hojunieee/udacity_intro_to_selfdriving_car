8b0d302f9bc98f75dc579a240629e4e0de134bbc
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-10 20:18:15 +0900

    Update daysBetweenDates.py

--- daysBetweenDates.py
+++ daysBetweenDates.py
@@ -10,3 +10,14 @@ def nextDay(year, month, day):
       return year, month+1,1
     else:
       return year+1,1,1
+
+    
+def dateIsBefore(Y1,M1,D1,Y2,M2,D2):
+  if Y1<Y2:
+    return True
+  if Y1==Y2:
+    if M1<M2:
+      return True
+    if M1==M2:
+      return D1<D2
+  return False
