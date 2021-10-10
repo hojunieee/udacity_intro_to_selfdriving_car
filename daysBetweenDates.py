bb6ad771bb6e5f59a72fcbf53c9985676b44760c
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-10 20:26:34 +0900

    Update daysBetweenDates.py

--- daysBetweenDates.py
+++ daysBetweenDates.py
@@ -1,4 +1,14 @@
-
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    assert not dateIsBefore(year2, month2, day2,year1, month1, day1)
+    
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
 
 
 def nextDay(year, month, day):
@@ -21,3 +31,5 @@ def dateIsBefore(Y1,M1,D1,Y2,M2,D2):
     if M1==M2:
       return D1<D2
   return False
+
+
