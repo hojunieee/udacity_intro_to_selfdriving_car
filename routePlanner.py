0b34a602c06b513bea653a79a445703724cb99d1
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-15 23:26:59 +0900

    Create routePlanner.py

---
+++ routePlanner.py
@@ -0,0 +1,76 @@
+# Do not change this cell
+# When you write your methods correctly this cell will execute
+# without problems
+class PathPlanner():
+    """Construct a PathPlanner Object"""
+    def __init__(self, M, start=None, goal=None):
+        """ """
+        self.map = M
+        self.start= start
+        self.goal = goal
+        self.closedSet = self.create_closedSet() if goal != None and start != None else None
+        self.openSet = self.create_openSet() if goal != None and start != None else None
+        self.cameFrom = self.create_cameFrom() if goal != None and start != None else None
+        self.gScore = self.create_gScore() if goal != None and start != None else None
+        self.fScore = self.create_fScore() if goal != None and start != None else None
+        self.path = self.run_search() if self.map and self.start != None and self.goal != None else None
+    
+    def reconstruct_path(self, current):
+        """ Reconstructs path after search """
+        total_path = [current]
+        while current in self.cameFrom.keys():
+            current = self.cameFrom[current]
+            total_path.append(current)
+        return total_path
+    
+    def _reset(self):
+        """Private method used to reset the closedSet, openSet, cameFrom, gScore, fScore, and path attributes"""
+        self.closedSet = None
+        self.openSet = None
+        self.cameFrom = None
+        self.gScore = None
+        self.fScore = None
+        self.path = self.run_search() if self.map and self.start and self.goal else None
+
+    def run_search(self):
+        """ """
+        if self.map == None:
+            raise(ValueError, "Must create map before running search. Try running PathPlanner.set_map(start_node)")
+        if self.goal == None:
+            raise(ValueError, "Must create goal node before running search. Try running PathPlanner.set_goal(start_node)")
+        if self.start == None:
+            raise(ValueError, "Must create start node before running search. Try running PathPlanner.set_start(start_node)")
+
+        self.closedSet = self.closedSet if self.closedSet != None else self.create_closedSet()
+        self.openSet = self.openSet if self.openSet != None else  self.create_openSet()
+        self.cameFrom = self.cameFrom if self.cameFrom != None else  self.create_cameFrom()
+        self.gScore = self.gScore if self.gScore != None else  self.create_gScore()
+        self.fScore = self.fScore if self.fScore != None else  self.create_fScore()
+
+        while not self.is_open_empty():
+            current = self.get_current_node()
+
+            if current == self.goal:
+                self.path = [x for x in reversed(self.reconstruct_path(current))]
+                return self.path
+            else:
+                self.openSet.remove(current)
+                self.closedSet.add(current)
+
+            for neighbor in self.get_neighbors(current):
+                if neighbor in self.closedSet:
+                    continue    # Ignore the neighbor which is already evaluated.
+
+                if not neighbor in self.openSet:    # Discover a new node
+                    self.openSet.add(neighbor)
+                
+                # The distance from start to a neighbor
+                #the "dist_between" function may vary as per the solution requirements.
+                if self.get_tentative_gScore(current, neighbor) >= self.get_gScore(neighbor):
+                    continue        # This is not a better path.
+
+                # This path is the best until now. Record it!
+                self.record_best_path_to(current, neighbor)
+        print("No Path Found")
+        self.path = None
+        return False
