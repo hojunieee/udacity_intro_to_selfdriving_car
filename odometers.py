b8212323ddb247ed82b498be8dbfcff24b52be88
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-23 23:17:03 +0900

    Create odometers.py

---
+++ odometers.py
@@ -0,0 +1,8 @@
+X = [2.000,2.333,2.667,3.000]
+Y = [30,40,68,80]
+plt.scatter(X,Y)
+plt.plot(X,Y)
+plt.title("Position vs. Time on a Roadtrip")
+plt.xlabel("Time (in hours)")
+plt.ylabel("Odometer Reading (in miles)")
+plt.show()
