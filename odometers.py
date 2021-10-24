af73f0f0e887f5708ab87954df0a6e134519c91c
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-10-24 23:01:36 +0900

    Update odometers.py

--- odometers.py
+++ odometers.py
@@ -45,3 +45,45 @@ def integral(f, t1, t2, dt=0.1):
         area += dA
         t += dt
     return area
+
+
+
+def get_integral_from_data(acceleration_data, times):
+    # 1. We will need to keep track of the total accumulated speed
+    accumulated_speed = 0.0
+    
+    # 2. The next lines should look familiar from the derivative code
+    last_time = times[0]
+    speeds = []
+    
+    # 3. Once again, we lose some data because we have to start
+    #    at i=1 instead of i=0.
+    for i in range(1, len(times)):
+        
+        # 4. Get the numbers for this index i
+        acceleration = acceleration_data[i]
+        time = times[i]
+        
+        # 5. Calculate delta t
+        delta_t = time - last_time
+        
+        # 6. This is an important step! This is where we approximate
+        #    the area under the curve using a rectangle w/ width of
+        #    delta_t.
+        delta_v = acceleration * delta_t
+        
+        # 7. The actual speed now is whatever the speed was before
+        #    plus the new change in speed.
+        accumulated_speed += delta_v
+        
+        # 8. append to speeds and update last_time
+        speeds.append(accumulated_speed)
+        last_time = time
+    return speeds
+
+# 9. Now we use the function we just defined
+integrated_speeds = get_integral_from_data(ACCELERATIONS, TIMESTAMPS)
+
+# 10. Plot
+plt.scatter(TIMESTAMPS[1:], integrated_speeds)
+plt.show()
