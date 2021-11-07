83ed414eb6e74c00938972ee9e5e5d2434535466
Hojunieee <84814259+hojunieee@users.noreply.github.com>
2021-11-07 23:01:26 +0900

    Update 8_image_as_gray_pixels.py

--- 8_image_as_gray_pixels.py
+++ 8_image_as_gray_pixels.py
@@ -17,7 +17,7 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 plt.imshow(gray_image, cmap='gray')
 
-
+#=========================================
 
 ## TODO: Define the color selection boundaries in HSV values
 
@@ -35,3 +35,59 @@ masked_image2[mask2 != 0] = [0]
 
 # Display it!
 plt.imshow(masked_image2)
+
+#=========================================
+#histogram
+def hsv_histograms(rgb_image):
+    # Convert to HSV
+    hsv = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
+
+    # Create color channel histograms
+    h_hist = np.histogram(hsv[:,:,0], bins=32, range=(0, 180))
+    s_hist = np.histogram(hsv[:,:,1], bins=32, range=(0, 256))
+    v_hist = np.histogram(hsv[:,:,2], bins=32, range=(0, 256))
+    
+    # Generating bin centers
+    bin_edges = h_hist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+
+    # Plot a figure with all three histograms
+    fig = plt.figure(figsize=(12,3))
+    plt.subplot(131)
+    plt.bar(bin_centers, h_hist[0])
+    plt.xlim(0, 180)
+    plt.title('H Histogram')
+    plt.subplot(132)
+    plt.bar(bin_centers, s_hist[0])
+    plt.xlim(0, 256)
+    plt.title('S Histogram')
+    plt.subplot(133)
+    plt.bar(bin_centers, v_hist[0])
+    plt.xlim(0, 256)
+    plt.title('V Histogram')
+    
+    return h_hist, s_hist, v_hist
+
+
+#=========================================
+#To sum up pixel values along the columns or rows of an image.
+
+# Convert the night image to HSV colorspace
+hsv_night = cv2.cvtColor(night_image, cv2.COLOR_RGB2HSV)
+
+# Isolate the V component
+v = hsv_night[:,:,2]
+
+# Sum the V component over all columns (axis = 0)
+v_sum = np.sum(v[:,:], axis=0)
+
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+
+ax2.set_title('Value sum over columns')
+ax1.plot(v_sum)
+
+ax2.set_title('Original image')
+ax2.imshow(night_image, cmap='gray')
+
+
